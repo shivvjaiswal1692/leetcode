922243a97ec740aa4e9b78b161f522f6206b83e9
shivvjaiswal1692 <contact.shivamj@gmail.com>
2026-06-23 18:15:15 +0530

    Added Two Sum - Two Pointers

---
+++ twosumii.java
@@ -0,0 +1,29 @@
+/*
+Problem: Two Sum II - Input Array Is Sorted
+LeetCode #: 167
+Difficulty: Medium
+
+Pattern: Two Pointers
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum==target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{};
+    }
+}
