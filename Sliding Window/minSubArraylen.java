02ff127a195fe2e2d20f97809596272b2a19a534
shivvjaiswal1692 <contact.shivamj@gmail.com>
2026-07-09 11:11:35 +0530

    Minimum Size Subarray Sum

---
+++ Sliding Window/minSubArraylen.java
@@ -0,0 +1,28 @@
+/* LeetCode - 209
+    Minimum Size Subarray Sum
+    Level - Medium
+    Approach - Sliding Window 
+    Time Complexity - O(n)
+    Space Complexity - O(1)
+*/
+
+public class MinSubarraylen {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++){
+            sum += nums[right];
+
+            while (sum >= target){
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+
+    }
+}
