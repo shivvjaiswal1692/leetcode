e112a713b6be47c7ebaddcab773db76c4cd4325b
shivvjaiswal1692 <contact.shivamj@gmail.com>
2026-07-11 00:10:13 +0530

    Longest Repeating Character Replacement

---
+++ Sliding Window/longestrepeatingcharacerreplacement.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class longestrepeatingcharacerreplacement {
+    public int characterReplacement(String s, int k) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            maxFreq = Math.max(maxFreq, map.get(ch));
+
+            while ((right - left + 1) - maxFreq > k) {
+
+                char leftChar = s.charAt(left);
+
+                map.put(leftChar, map.get(leftChar) - 1);
+
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
