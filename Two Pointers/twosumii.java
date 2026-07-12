a4d1db776e47558335fc1ed64f59f87c393916cd
shivvjaiswal1692 <contact.shivamj@gmail.com>
2026-07-12 09:42:06 +0530

    updated the class name to match the file name

--- Two Pointers/twosumii.java
+++ Two Pointers/twosumii.java
@@ -9,7 +9,7 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-class Solution {
+public class twosumii{
     public int[] twoSum(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1;
 
