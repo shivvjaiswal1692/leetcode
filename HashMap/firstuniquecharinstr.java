e0b5037b4326c12ee758998c743212a2cb0e4ecf
shivvjaiswal1692 <contact.shivamj@gmail.com>
2026-07-22 13:43:21 +0530

    First unique character in a string

---
+++ HashMap/firstuniquecharinstr.java
@@ -0,0 +1,23 @@
+/*  LC - 387
+    First Unique Character in a String
+    Level - Easy
+    Approach - HashMap
+*/    
+
+public class firstuniquecharinstr {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
